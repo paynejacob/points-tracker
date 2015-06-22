346a9d43e0b12e4e58548789f9ba3ac840ee11f9
bll-z <bll-z@Williams-MacBook-Pro.local>
2015-06-21 22:30:49 -0400

    an angular flask app

--- manage.py
+++ manage.py
@@ -1,13 +1,64 @@
-from flask.ext.script import Manager
-from points_tracker.app import app
+#!/usr/bin/env python
+import os
+import sqlalchemy
+from flask_script import Manager, Shell, Server
+from flask_migrate import MigrateCommand
 
+from points_tracker.settings import get_config_for_current_environment
+
+from points_tracker.app import create_app
+from points_tracker.auth.models import User
+from points_tracker.models import *
+from points_tracker.database import db
+
+app = create_app(get_config_for_current_environment())
 manager = Manager(app)
 
-# overriding default runserver to enable debugging
-@manager.command
-def runserver():
-    app.debug = True
-    app.run()
+def _make_context():
+    """Return context dict for a shell session so you can access
+    app, db, and the User model by default.
+    """
+    return {'app': app, 'db': db, 'User': User}
 
-if __name__ == "__main__":
+@manager.command
+def test():
+    """Run the tests."""
+    import pytest
+    HERE = os.path.abspath(os.path.dirname(__file__))
+    TEST_PATH = os.path.join(HERE, 'tests')
+    exit_code = pytest.main([TEST_PATH, '--verbose'])
+    return exit_code
+
+@manager.command
+def create_db():
+    connection_string = '%s/postgres' % app.config['SQLALCHEMY_DATABASE_URI'].rsplit('/', 1)[0]
+    with sqlalchemy.create_engine(connection_string, isolation_level='AUTOCOMMIT').connect() as connection:
+        try:
+            connection.execute('CREATE DATABASE {}'.format('points_tracker'))
+            print 'Created database {}'.format('points_tracker')
+        except Exception, ex:
+            # fails if db already exists
+            print ex.message
+
+@manager.command
+def create_user():
+    import getpass
+    user = raw_input("Username [{}]: ".format(getpass.getuser()))
+    if not user:
+        user = getpass.getuser()
+    pprompt = lambda: (getpass.getpass(), getpass.getpass('Retype password: '))
+    p1, p2 = pprompt()
+    while p1 != p2:
+        print('Passwords do not match. Try again')
+        p1, p2 = pprompt()
+
+    User.create(username=user, password=p1, active=True, is_admin=True)
+    print 'Administrator account created for {}'.format(user)
+
+
+manager.add_command('server', Server(threaded=True))
+manager.add_command('shell', Shell(make_context=_make_context))
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
     manager.run()
---
+++ points_tracker/static/js/points_tracker.js
@@ -0,0 +1,40 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('points_tracker')
+    .config(PointsConfig)
+    .run(PointsRun);
+
+  PointsConfig.$inject = ['$stateProvider', '$urlRouterProvider', '$resourceProvider'];
+
+  function PointsConfig($stateProvider, $urlRouterProvider, $resourceProvider) {
+    $urlRouterProvider.otherwise("/");
+
+    // Normal analyst states
+    $stateProvider
+      .state('dashboard', {
+        url: '/',
+        templateUrl: '/static/partials/dashboard.html',
+      });
+
+    // Don't strip trailing slashes from calculated URLs
+    $resourceProvider.defaults.stripTrailingSlashes = false;
+
+  }
+
+  PointsRun.$inject = ['$rootScope'];
+
+  function PointsRun($rootScope) {
+    // Expose `_.` methods in any template
+    $rootScope._ = _;
+
+    $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+      // Scroll to top of page on any state change.
+      // This is coupled with the `autoscroll="false"` on the ui-view tag
+      // in points_tracker/templates/dashboard.html
+      $('html, body').animate({scrollTop: 0}, 200);
+
+    });
+  }
+})();
